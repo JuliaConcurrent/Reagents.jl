a2b03169403d7f181d4f6f0f2aa9b4ef8f63b7d4
Takafumi Arakaki <aka.tkf@gmail.com>
2021-08-02 11:59:37 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/Reagents.jl
@@ -0,0 +1,11 @@
+baremodule Reagents
+
+module Internal
+
+using ..Reagents: Reagents
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule Reagents
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Reagents
+using Test
+
+@testset "Reagents.jl" begin
+    # Write your tests here.
+end
